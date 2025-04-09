4678afef31d2e09d084117b8a677002d94df919b
Naing Grace <grvce.n@gmail.com>
2025-04-09 14:56:57 +0200

    Tache 5 terminée

--- src/main/scala/LinkedList.scala
+++ src/main/scala/LinkedList.scala
@@ -1,5 +1,7 @@
-class LinkedList {
-  var head: Node = null
+import java.security.KeyStore.TrustedCertificateEntry
+
+class LinkedList(var head: Node = null) {
+
 
   def addToStart(s: String): Unit ={
     head = new Node(s, head)
@@ -13,6 +15,33 @@ class LinkedList {
       tmp = tmp.next
     }
     return count
+  }
+
+  def removeFirstElement(): Unit = {
+   this.head = this.head.next
+  }
+
+  def getLastElement(): Node = {
+    var tmp : Node = this.head
+    for (i <- 0 until this.getSize -1){ // -1 : avant dernier (last = null)
+      tmp = tmp.next
+    }
+    return tmp
+
+//    recursive method
+//    if(head == null) return null
+//    if(head.next == null) return head
+//    new LinkedList(head.next).getLastElement()  -> need a constructor
+  }
+
+  def addtoEnd(e: String): Unit = {
+    getLastElement().next = new Node(e, null)
+  }
+
+  def isPresent(e: String): Boolean = {
+    if(head == null) return false
+    if (head.item == e) return true
+    new LinkedList(head.next).isPresent(e)
   }
 
   override def toString: String = {
