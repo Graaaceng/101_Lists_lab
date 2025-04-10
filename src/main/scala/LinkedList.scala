397ae841c4d7cc30f10eb1be02bf3be4a418c207
Naing Grace <grvce.n@gmail.com>
2025-04-10 15:15:09 +0200

    Dernières tâches terminées

--- src/main/scala/LinkedList.scala
+++ src/main/scala/LinkedList.scala
@@ -2,7 +2,6 @@ import java.security.KeyStore.TrustedCertificateEntry
 
 class LinkedList(var head: Node = null) {
 
-
   def addToStart(s: String): Unit ={
     head = new Node(s, head)
   }
@@ -18,7 +17,8 @@ class LinkedList(var head: Node = null) {
   }
 
   def removeFirstElement(): Unit = {
-   this.head = this.head.next
+    if (head == null) return null
+    head = head.next
   }
 
   def getLastElement(): Node = {
@@ -34,15 +34,53 @@ class LinkedList(var head: Node = null) {
 //    new LinkedList(head.next).getLastElement()  -> need a constructor
   }
 
-  def addtoEnd(e: String): Unit = {
+  def addToEnd(e: String): Unit = {
+    if (getLastElement() == null) head = new Node(e, null)
     getLastElement().next = new Node(e, null)
   }
 
   def isPresent(e: String): Boolean = {
-    if(head == null) return false
-    if (head.item == e) return true
+    if(this.head == null) return false
+    if(this.head.item == e) return true
     new LinkedList(head.next).isPresent(e)
   }
+
+  def findElement(e:String): Node = {
+    if (this.head == null) return null
+    if (this.head.item == e) return head
+    new LinkedList(head.next).findElement(e)
+  }
+
+  def swapElements(e1: String, e2: String): Unit = {
+    if(this.isPresent(e1) && this.isPresent(e2)){
+      findElement(e1).item = e2
+      findElement(e2).item = e1
+    }
+  }
+
+  def removeLastElement(): Unit = {
+    if (head == null) return null
+    var tmp : Node = this.head
+    for (i <- 0 until this.getSize -2){
+      tmp = tmp.next
+    }
+    tmp.next = null
+  }
+
+  def removeElement(e: String): Unit = {
+    if (this.isPresent(e)) {
+      if (head == findElement(e)) head = head.next
+      if (head.next == findElement(e)) head.next = head.next.next
+      new LinkedList(head.next).removeElement(e)
+    }
+  }
+
+  def insertAfter(before: String, after: String): Unit ={
+    if (this.isPresent(before)){
+      findElement(before).next = new Node(after, findElement(before).next)
+    }
+  }
+
 
   override def toString: String = {
     var list: String = ""
@@ -57,6 +95,7 @@ class LinkedList(var head: Node = null) {
 
 object LinkedList extends App {
   var flightList:LinkedList = new LinkedList()
+
   println(flightList)
   flightList.addToStart("Rome")
   println(flightList)
@@ -64,4 +103,10 @@ object LinkedList extends App {
   println(flightList)
   flightList.addToStart("Tokyo")
   println(flightList)
-  }
+  flightList.insertAfter("Paris", "Lisbon")
+  flightList.insertAfter("Lisbon", "Zurich")
+  flightList.addToEnd("Oslo")
+  println(flightList)
+  flightList.removeElement("Tokyo")
+  println(flightList)
+}
--- src/main/scala/Node.scala
+++ src/main/scala/Node.scala
@@ -1,3 +1,3 @@
-class Node(val item:String, var next:Node)
+class Node(var item:String, var next:Node)
 
 //var list = new Node("Tokyo", new Node("Paris", new Node("Milan", null)))
